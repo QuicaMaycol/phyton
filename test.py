29afc30f413fc97805f39ad8d762bf6d0921c943
Maycol Quicaño <97490339+QuicaMaycol@users.noreply.github.com>
2025-02-18 22:58:34 -0500

    test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import sys
-print("Python está instalado, versión:", sys.version)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "¡Hola! Render está funcionando correctamente 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
