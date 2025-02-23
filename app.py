0643461acd0c4d8d68d96810799c4650278b6ad8
Maycol Quicaño <97490339+QuicaMaycol@users.noreply.github.com>
2025-02-22 19:07:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ cada respuesta que des tiene que ser corta con un maximo de 30 palabras
         # Generar audio con ElevenLabs
         audio_stream = client_elevenlabs.text_to_speech.convert(
             text=respuesta_ia,
-            stability: 0.5,
+            stability: "0.5",
             model="eleven_multilingual_v2",  # Asegura que estás usando un modelo compatible 
             voice_id=VOICE_ID
            
